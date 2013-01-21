f16a41215cc1972cc060d1e3a4d4c3d4d7a1ab69
Sascha Ziemann <ceving@gmail.com>
2013-01-21 13:09:28 +0100

    x

--- finalizer.scm
+++ finalizer.scm
@@ -2,7 +2,7 @@
 
 (load "./finalizer.so")
 
-(system "find /proc/$PPID/fd -ls|sed s%.*/proc%%")
 (XOpenDisplay #f)
+(system "find /proc/$PPID/fd -ls|sed s%.*/proc%%")
 (gc)
 (system "find /proc/$PPID/fd -ls|sed s%.*/proc%%")
